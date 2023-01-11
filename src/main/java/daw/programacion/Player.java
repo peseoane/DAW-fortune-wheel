491fcf7c2c1bf90ff4a4f8db2dd94c7951aaf0cc
AmelyECH <116146886+amelych@users.noreply.github.com>
2023-01-11 10:24:27 +0100

    clase player

---
+++ src/main/java/daw/programacion/Player.java
@@ -0,0 +1,48 @@
+package daw.programacion;
+
+public class Player {
+    private String name;
+    private int money;
+    private boolean turn;
+
+    /**
+     * En caso de que inicie el juego, el dinero empieza en 0
+     * @param name nombre jugador
+     */
+    public Player(String name) {
+        this.name = name;
+        this.money = 0;
+    }
+
+    /**
+     * Constructor cuando un jugador mantiene dinero de otra ronda.
+     * @param name nombre de jugador
+     * @param money sobrecarga, en caso de tener dinero especificarlo
+     */
+    public Player(String name, int money) {
+        this.name = name;
+        this.money = money;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getMoney() {
+        return money;
+    }
+
+    public void setMoney(int money) {
+        this.money = money;
+    }
+
+    //public int buyVocal() {
+    //    if (money >= ) {
+
+    //    }
+    //}
+}
