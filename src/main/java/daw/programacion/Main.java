1694ef62a9ed7f26a562cf874d4e78b1ede63adc
Pedro Seoane Prado <info@pedroseoaneprado.es>
2023-01-19 09:28:37 +0100

    Representación de la ruleta funcional con centrado, método extraído

--- src/main/java/daw/programacion/Main.java
+++ src/main/java/daw/programacion/Main.java
@@ -1,7 +1,8 @@
 package daw.programacion;
 
 public class Main {
+
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println("Hello, world!");
     }
 }
