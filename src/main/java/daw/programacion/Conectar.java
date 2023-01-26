50b76d55e747b059916a7d92f1576c3ee15399d3
Pedro Seoane Prado <info@pedroseoaneprado.es>
2023-01-26 09:46:49 +0100

    SQLite prototipo funcional

--- src/main/java/daw/programacion/Conectar.java
+++ src/main/java/daw/programacion/Conectar.java
@@ -9,13 +9,22 @@ public class Conectar {
 
   public static String conectar(String SQL) {
     Connection conn = null;
+    StringBuilder enunciado = new StringBuilder();
+
     try {
       // create a connection to the database
       conn = DriverManager.getConnection(DATABASE_URL);
       Statement stmt = conn.createStatement();
       ResultSet rs = stmt.executeQuery(SQL);
-        System.out.println(rs.getString("enigma"));
+      // from rs, the SQL print all the data
+        while (rs.next()) {
+          if (rs.getString("contestada").equals("0")) {
+            // System.out.println(rs.getString("enunciado"));
+            enunciado = new StringBuilder(rs.getString("enunciado"));
+          }
+        }
       System.out.println("Connection to SQLite has been established.");
+
     } catch (SQLException e) {
       System.out.println(e.getMessage());
     } finally {
@@ -26,7 +35,8 @@ public class Conectar {
       } catch (SQLException ex) {
         System.out.println(ex.getMessage());
       }
+
     }
-    return null;
+    return enunciado.toString();
   }
 }
--- src/main/java/daw/programacion/Main.java
+++ src/main/java/daw/programacion/Main.java
@@ -4,6 +4,7 @@ public class Main {
 
   public static void main(String[] args) {
     System.out.println("Hello, world!");
-    Conectar.conectar("SELECT * FROM enigmas");
+    String resultado = Conectar.conectar("SELECT * FROM enigmas;");
+    System.out.println(resultado);
   }
 }
