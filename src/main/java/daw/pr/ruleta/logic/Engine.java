b59915f13d53bbb9a0741465aa983406dcad133b
Pedro Seoane Prado <info@pedroseoaneprado.es>
2023-02-15 09:53:41 +0100

    Changes by a20amelych

--- src/main/java/daw/pr/ruleta/logic/Engine.java
+++ src/main/java/daw/pr/ruleta/logic/Engine.java
@@ -73,4 +73,52 @@ public class Engine {
             return false;
         }
     }
+
+    /**
+     * El método premio se encarga de asignar al jugador su premio en función de la casilla en la que haya caído.
+     * Si el jugador cae en la casilla de quiebra, pierde all su dinero. Si cae en la casilla de x2, duplica su dinero.
+     * Si cae en la casilla de 1/2, da la mitad de su dinero a la banca.
+     * Si cae en la casilla de pierde turno y el jugador no tiene un comodín pierde su turno, si tiene un comodín no
+     * pierde su turno y se le quita ese comodín al jugador. Si cae en la casilla de comodín, se le añade un comodín.
+     */
+    public void Premio() {
+        String premio = player.getCasillaRuleta();
+        
+        int premioInt = 0;
+        int comodin = 0;
+        try {
+            premioInt = Integer.parseInt(premio);
+        } catch (NumberFormatException e) {
+            logger.info("Premio especial");
+        }
+
+        if (premioInt != 0) {
+            player.setMoney(player.getMoney() + premioInt);
+        }
+        else {
+            if (premio.equals("Quiebra")) {
+                player.setMoney(0);
+            }
+            else if (premio.equals("x2")) {
+                player.setMoney(player.getMoney() * 2);
+            }
+            else if (premio.equals("1/2")) {
+                player.setMoney(player.getMoney() / 2);
+            }
+            else if (premio.equals("Comodín")) {
+                comodin++;
+            }
+            else if (premio.equals("Pierde turno")) {
+                boolean continuar = player.isTurn();
+                if (comodin > 0) {
+                    continuar = true;
+                    comodin--;
+                } else {
+                    continuar = false;
+                }
+            }
+        }
+
+
+    }
 }
