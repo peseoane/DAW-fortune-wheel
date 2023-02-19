2aed894f35466213e28479c1c0c9c77f693503c7
Pedro Vicente Seoane Prado <info@pedroseoaneprado.es>
2023-02-19 18:50:35 +0100

    Splash screen? maybe

--- src/main/java/daw/pr/ruleta/Main.java
+++ src/main/java/daw/pr/ruleta/Main.java
@@ -1,7 +1,20 @@
 package daw.pr.ruleta;
 
+import daw.pr.ruleta.logic.Engine;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
 public class Main {
+    static Logger logger = LogManager.getLogger(Main.class);
+
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+
+        int turno = 0;
+
+        Engine engine = new Engine();
+
+        
+        engine.getNumeroJugadores();
+
     }
 }
